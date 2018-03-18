eb44ceb74b87e8294ced2ca7cc2cc7d6f450ff37
Ralf S. Engelschall <rse@engelschall.com>
2018-03-18 19:58:27 +0100

    fix name

--- Gruntfile.js
+++ Gruntfile.js
@@ -87,7 +87,7 @@ module.exports = function (grunt) {
             }
         },
         mochaTest: {
-            "stdarg": {
+            "textframe": {
                 src: [ "tst/*.js" ]
             },
             options: {
